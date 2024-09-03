5d17292a86ea6bdac9fe9175588246d263bcb39e
Kayron Mendes <kayronmend@gmail.com>
2024-09-03 17:08:44 -0300

    Trabalhando com input no python

---
+++ atividade1.py
@@ -0,0 +1,10 @@
+nomefazenda = (input("Qual o nome da sua fazendidnha?"))
+qtdcavalos = float(input("Qual é a sua quantidade de cavalos?"))
+
+
+quantidade_ferraduras = (qtdcavalos*4)
+
+print(f"A quantidade de ferraduras que você precisa é {quantidade_ferraduras}")
+
+print(f"A quantidade de ferraduras que você precisa é  {quantidade_ferraduras:.2f}")
+
