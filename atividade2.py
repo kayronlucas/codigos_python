4c65a7540924ab9fca4bd16b8670ffcc6c6d7386
Kayron Mendes <kayronmend@gmail.com>
2024-09-03 17:35:51 -0300

    Questão da idade

---
+++ atividade2.py
@@ -0,0 +1,7 @@
+ano_idade = float(input("Informe sua idade em anos:"))
+
+meses_idade = ano_idade*12
+dia_idade = ano_idade*365
+
+print(f"A idade em meses é: {meses_idade}")
+print(f"A idade em dias é: {dia_idade}")
